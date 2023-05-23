5e576d30e1188d928cb22df385f6a84ae32db2b2
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2023-05-23 05:44:53 +0000

    initial commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,7 +12,80 @@ const Projects = () => {
         backgroundSize: "100% 100%",
       }}
     >
-      <div>Projects</div>
+      <div className="project">
+        <img src="" alt="icon" />
+        <div className="projectDescription">
+          <p className="title">Vasavi Voices</p>
+          <p className="description">
+            A college-level podcast website to connect alumni experiences and
+            their valuable suggestions with future alumni.
+          </p>
+          <div className="urls">
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit repository
+            </a>
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit Project
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <img src="" alt="icon" />
+        <div className="projectDescription">
+          <p className="title">
+            Mobile Price Prediction Using Random Forest Algorithm
+          </p>
+          <p className="description">
+            A web application developed with a Machine Learning model to suggest
+            prices of Mobile Phones based on the features of mobile phone.
+          </p>
+          <div className="urls">
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit repository
+            </a>
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit Project
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <img src="" alt="icon" />
+        <div className="projectDescription">
+          <p className="title">The Tianzhu</p>
+          <p className="description">
+            A web application with a user-friendly design that can give a quick
+            summary of all Indian state cultures.
+          </p>
+          <div className="urls">
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit repository
+            </a>
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit Project
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <img src="" alt="icon" />
+        <div className="projectDescription">
+          <p className="title">E – Plastic Management System</p>
+          <p className="description">
+            A web application that resembles an idea to reduce the wastage of
+            plastic in society.
+          </p>
+          <div className="urls">
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit repository
+            </a>
+            <a href="https://www.google.com" target="__blank">
+              <i class="fa-solid fa-globe"></i>Visit Project
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
